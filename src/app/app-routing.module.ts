69d64d8b79e43994993b149b16bb9a07fcf38b98
Yogesh Urade <47246796+maya456789@users.noreply.github.com>
2024-08-10 19:24:17 +0530

    sec cmt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  // {path:'',component:LoginComponent},
-  // {path:'signup',component:RegisterComponent}
+  
 ];
 
 @NgModule({
